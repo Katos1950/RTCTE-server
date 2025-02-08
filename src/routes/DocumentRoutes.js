16b3f7f24459d906314cadee0dde631de272ed6c
Kratos1950 <87760385+Katos1950@users.noreply.github.com>
2025-02-08 13:21:00 -0600

    Renaming document

--- src/routes/DocumentRoutes.js
+++ src/routes/DocumentRoutes.js
@@ -56,4 +56,24 @@ router.delete("/documents/del",authenticateToken, async (req,res)=>{
     }
 })
 
+router.put("/documents/rename",authenticateToken, async (req,res)=>{
+    try{
+        const result = await DocModel.updateOne(
+            {name:req.body.name,
+            createdBy:req.user.emailId},
+            { $set: { name: req.body.newName } } 
+        )
+        
+        if (result.modifiedCount === 0) {
+            return res.status(404).json({ message: "Entry not found" });
+        }
+
+        res.status(200).json({message:"Entry updated"})
+        
+    }
+    catch(error){
+        res.status(500).json({error:error.message})
+    }
+})
+
 module.exports = router;
