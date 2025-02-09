912281c0924277153ae75571b822c14e5e8c2e0a
Kratos1950 <87760385+Katos1950@users.noreply.github.com>
2025-02-09 16:44:13 -0600

    Send active users to frontend

--- textEditorServer.js
+++ textEditorServer.js
@@ -11,16 +11,29 @@ const io = require("socket.io")(3001, {
     }
 });
 
+let activeUsers={}
+
 io.on("connection", socket => {
-    socket.on("get-document", async documentId => {
+    let userEmail = null; // Variable to store the user's email
+    socket.on("get-document", async ({ documentId, emailId }) => {
         if (!documentId) return;
         
         let document = await Document.findById(documentId);
         if (!document) {
             document = await Document.create({ _id: documentId, content: "" });
         }
-        
+
         socket.join(documentId);
+
+        if(!activeUsers[documentId]) {
+            activeUsers[documentId]= new Set();
+        }
+        activeUsers[documentId].add(emailId)
+        userEmail = emailId;
+        console.log(activeUsers)
+        io.to(documentId).emit("update-active-users", Array.from(activeUsers[documentId]));
+
+
         socket.emit("load-document", document.content);
 
         socket.on("send-changes", delta => {
@@ -34,5 +47,16 @@ io.on("connection", socket => {
 
     socket.on("disconnect", () => {
         console.log("User disconnected");
+        if (userEmail) {
+            // Iterate through active users and remove the user's email
+            for (let documentId in activeUsers) {
+                if (activeUsers[documentId].has(userEmail)) {
+                    activeUsers[documentId].delete(userEmail);
+                    console.log(`Removed ${userEmail} from document ${documentId}`);
+                    io.to(documentId).emit("update-active-users", Array.from(activeUsers[documentId]));
+                }
+            }
+        }
+        console.log(activeUsers); // Log active users after disconnection
     });
 });
